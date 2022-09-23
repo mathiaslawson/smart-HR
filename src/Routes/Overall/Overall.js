4dfbd8e5db53188394e4704783d120a0d6739f3d
unknown <mathiaslawson@gmail.com>
2022-09-23 13:02:06 +0100

    graph stats & without transfered data

--- src/Routes/Overall/Overall.js
+++ src/Routes/Overall/Overall.js
@@ -3,10 +3,7 @@ import './Overall.css'
 
 import base from '../Enroll/Base';
 
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts'
-
-const data = [{name: 'Page A', uv: 400, pv:2400, amt:2400 } ];
-
+import { RadialBarChart, RadialBar,BarChart, Tooltip, Legend, Bar, CartesianGrid, XAxis, YAxis } from 'recharts'
 
 
 const list = base.map((member)=>{
@@ -20,8 +17,74 @@ const list = base.map((member)=>{
 </button>
 })
 
-console.log(base)
+const data = [
+    {
+      "name": "Work Ethics (2021/2022)",
+      "uv": 4000,
+      "pv": 2400
+    },
+    {
+      "name": "Completed Projects (2021/2022)",
+      "uv": 3000,
+      "pv": 1398
+    },
+    {
+      "name": "Reporting Days(2021/2022)",
+      "uv": 2000,
+      "pv": 9800
+    },
+    {
+      "name": "Interpersonal Work Relations(2021/2022",
+      "uv": 2780,
+      "pv": 3908
+    }
+  ]
 
+  const data2 = [
+    {
+      "name": "18-24",
+      "uv": 31.47,
+      "pv": 2400,
+      "fill": "#8884d8"
+    },
+    {
+      "name": "25-29",
+      "uv": 26.69,
+      "pv": 4567,
+      "fill": "#83a6ed"
+    },
+    {
+      "name": "30-34",
+      "uv": -15.69,
+      "pv": 1398,
+      "fill": "#8dd1e1"
+    },
+    {
+      "name": "35-39",
+      "uv": 8.22,
+      "pv": 9800,
+      "fill": "#82ca9d"
+    },
+    {
+      "name": "40-49",
+      "uv": -8.63,
+      "pv": 3908,
+      "fill": "#a4de6c"
+    },
+    {
+      "name": "50+",
+      "uv": -2.63,
+      "pv": 4800,
+      "fill": "#d0ed57"
+    },
+    {
+      "name": "unknow",
+      "uv": 6.67,
+      "pv": 4800,
+      "fill": "#ffc658"
+    }
+  ]
+  
 
 function Overall() {
   return (
@@ -36,20 +99,52 @@ function Overall() {
                  </div>
          </div>
          <div>
-            Profile Stats 
+         <BarChart width={1200} height={450} data={data}>
+  <CartesianGrid strokeDasharray="3 3" />
+  <XAxis dataKey="name" />
+  <YAxis />
+  <Tooltip />
+  <Legend />
+  <Bar dataKey="pv" fill="#8884d8" />
+  <Bar dataKey="uv" fill="#82ca9d" />
+</BarChart>
+            
          </div>
-         <div>
-            Profiler
-         </div>
-    </div>    
 
+    </div>  
+    <div className='radial'>
+      <div>
+        <RadialBarChart 
+  width={630} 
+  height={450} 
+  innerRadius="10%" 
+  outerRadius="80%" 
+  data={data2} 
+  startAngle={180} 
+  endAngle={0}
+>
+  <RadialBar minAngle={15} label={{ fill: '#666', position: 'insideStart' }} background clockWise={true} dataKey='uv' />
+  <Legend iconSize={10} width={120} height={140} layout='vertical' verticalAlign='middle' align="right" />
+  <Tooltip />
+</RadialBarChart>  
 </div>
-{/* <LineChart width={600} height={300} data={data}>
-    <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-    <CartesianGrid stroke="#ccc" />
-    <XAxis dataKey="name" />
-    <YAxis />
-  </LineChart> */}
+<div>
+        <RadialBarChart 
+  width={630} 
+  height={450} 
+  innerRadius="10%" 
+  outerRadius="80%" 
+  data={data2} 
+  startAngle={180} 
+  endAngle={0}
+>
+  <RadialBar minAngle={15} label={{ fill: '#666', position: 'insideStart' }} background clockWise={true} dataKey='uv' />
+  <Legend iconSize={10} width={120} height={140} layout='vertical' verticalAlign='middle' align="right" />
+  <Tooltip />
+</RadialBarChart>  
+</div>
+        </div> 
+</div>
 </>
 
     
